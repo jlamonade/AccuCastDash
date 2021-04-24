7d6c06eab71aa788c05e381207962958b7895d08
Jason <nyjasonlam@gmail.com>
2021-04-23 22:55:22 -0400

    Added jquery cdn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
-var mainLocationEl = $(".main-location")
-var mainConditionEl = $('.main-condition')
-var mainTemperatureEl = $('.main-temperature')
-var mainHighTempSpan = $('#main-high-temp')
-var mainLowTempSpan = $('#main-low-temp')
+var mainLocationEl = $(".main-location");
+var mainConditionEl = $(".main-condition");
+var mainTemperatureEl = $(".main-temperature");
+var mainHighTempSpan = $("#main-high-temp");
+var mainLowTempSpan = $("#main-low-temp");
 
 function getCurrentWeatherData() {
-    var apiKey = "5522e24e3f2cbcf4ca631dd68ebac697"
-    var cityName = 'new+york'
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+  var apiKey = "5522e24e3f2cbcf4ca631dd68ebac697";
+  var cityName = "new+york";
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 
-    fetch(requestUrl)
+  fetch(requestUrl)
     .then(function (response) {
-        return response.json()
+      return response.json();
     })
     .then(function (data) {
-        var weatherData = data;
-    })
+      var weatherData = data;
+    });
 }
 
-getCurrentWeatherData()
+getCurrentWeatherData();
