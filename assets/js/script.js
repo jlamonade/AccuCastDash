223e291045e5e1d7c2d2c9392b07c98120f70046
Jason <nyjasonlam@gmail.com>
2021-04-23 22:51:45 -0400

    Added getCurrentWeatherData() and dependencies

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var mainLocationEl = $(".main-location")
+var mainConditionEl = $('.main-condition')
+var mainTemperatureEl = $('.main-temperature')
+var mainHighTempSpan = $('#main-high-temp')
+var mainLowTempSpan = $('#main-low-temp')
+
+function getCurrentWeatherData() {
+    var apiKey = "5522e24e3f2cbcf4ca631dd68ebac697"
+    var cityName = 'new+york'
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        var weatherData = data;
+    })
+}
+
+getCurrentWeatherData()
