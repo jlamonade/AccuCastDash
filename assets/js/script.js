b73d9c657faa3a02e054c030d55712f2fc7b1891
Jason Lam <nyjasonlam@gmail.com>
2021-04-25 16:47:58 -0400

    Added https to requestUrl's

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ function getCoordinatesFromCityNameThenCallApi(city) {
 
 	the data validation helps filter out erroneous input, anything that isn't the name of a city
 	*/
-  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
